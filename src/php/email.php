08e9003aae3d07adfeba6c60fc0460c55c823cb2
Enricco Bertazzo <enriccowhz@gmail.com>
2021-09-02 21:35:52 -0300

    Busca de emails recebidos

--- src/php/email.php
+++ src/php/email.php
@@ -1,5 +1,18 @@
 <?php
 
+session_start();
 
+include('connect.php');
+include('enviar.php');
+
+$sql = "SELECT * FROM emails WHERE email LIKE '$para'";
+
+$resultado_emails = mysqli_query($connect, $sql);
+
+while($rows_emails = mysqli_fetch_array($resultado_emails)){
+    echo "Remetente: ".$rows_cursos['remetente'];
+    echo "Assunto: ".$rows_cursos['assunto'];
+    echo "Mensagem: ".$rows_cursos['mensagem'];
+}
 
 ?>
